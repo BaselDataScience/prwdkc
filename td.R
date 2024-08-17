561e65c821c020a1bf98212dd441c8df49d2d914
Reinhold <8436725+reikoch@users.noreply.github.com>
2024-08-17 16:13:52 +0200

    algorithm 2 added

---
+++ td.R
@@ -0,0 +1,19 @@
+# find diffusion time for cluster separation
+
+library(fpc)
+
+td_opt <- function(X, W, k, J) {
+  P <- W / rowSums(W)  # Transition matrix
+  xi <- colSums(P)  # ξ = νTP
+
+  V <- matrix(nrow = J+1, ncol = nrow(W))
+  P_1 <- diag(1/(xi+1)) %*% (P + t(P))
+  for (j in 1:(J+1)) {
+    V[j,] <- kmeans(P_1, k=k)$cluster
+    P_1 <- P_1 %*% P_1
+  }
+  
+  ch <- apply(V, 1, function(Y) fpc::calinhara(x=X, clustering = Y))
+  jmax <- which.max(ch)
+  return(2^(jmax-1))
+}
