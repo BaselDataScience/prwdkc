74a30e28041afc918a6562e19716eba9ccb471f2
Reinhold <8436725+reikoch@users.noreply.github.com>
2024-09-04 14:42:21 +0200

    allow start vector to be of length 1

--- R/diff_kernel.R
+++ R/diff_kernel.R
@@ -18,8 +18,8 @@ diff_kernel <- function(W, nu, ld) {
     stop("W must be a numeric matrix with non-negative entries")
   }
 
-  if (!is.numeric(nu) || length(nu) != nrow(W) || any(nu <= 0)) {
-    stop("nu must be a positive numeric vector with length equal to the number of rows in W")
+  if (!is.numeric(nu) || !length(nu) %in% c(1,nrow(W)) || any(nu <= 0)) {
+    stop("nu must be a positive numeric vector with length 1 or length equal to the number of rows in W")
   }
 
   if (!is.numeric(ld) || length(ld) != 1 || ld < 0 || ld != round(ld)) {
@@ -30,7 +30,7 @@ diff_kernel <- function(W, nu, ld) {
   # Main function body
   # Step 1: Compute the parametrized random walk operator P(ν)
   P <- W / Matrix::rowSums(W)  # Transition matrix
-  xi <- as.vector(Matrix::crossprod(nu, P))  # ξ = νTP
+  xi <- as.vector(Matrix::crossprod(nu, P))
   P_nu <- diag(1/(1 + xi/nu)) %*% (P + Matrix::tcrossprod(diag(1/nu), P) %*% diag(nu))
 
   # Step 2: Compute the parametrized random walk diffusion kernel
--- R/optcluster.R
+++ R/optcluster.R
@@ -1,4 +1,13 @@
-optcluster <- function(C, X) {
+#' find the optimal clustering
+#'
+#' @param X data matrix, rows are the vertices to be clustered
+#' @param C matrix, each column is one clustering result
+#'
+#' @return optimal clustering vector according to the Calinski-Harabasz index
+#' @export
+#'
+#' @examples
+optcluster <- function(X, C) {
   ch <- apply(C, 2, function(Y) fpc::calinhara(x=C, clustering = Y))
   C[,which.max(ch)]
 }
